4ed122ec8d749f8f069b5aaf462b331d8f0caf11
Suchet Iyer <iyer.suchet@gmail.com>
2020-05-18 12:48:21 +0530

    First commit, completed upto installing yargs

---
+++ app.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const chalk = require('chalk')
+const yargs = require('yargs')
+
+const getNotes = require('./getNotes.js')
+
+const log = console.log
+
+log(process.argv)
+log(yargs.argv)
+
+/*
+
+
+
+*/
+// const fs = require('fs');
+// const add = require('./utils.js');
+
+// const sum = add(3, 5);
+
+// console.log(sum);
+// fs.writeFileSync('notes.txt', 'This is the notes text document.');
+// fs.appendFileSync('notes.txt', `\nThe Author is ${namedoc}.`);
---
+++ getNotes.js
@@ -0,0 +1,24 @@
+console.log('getting notes..');
+
+const getNotes = function () {
+    const message = "Your Notes.";
+    return message;
+}
+
+module.exports = getNotes;
+/*
+
+
+
+*/
+// console.log("utils.js called");
+
+// const firstname = 'Suchet';
+
+// const lastname = 'Iyer';
+
+// const add = function (a, b) {
+//     return a + b;
+// }
+
+// module.exports = add;
